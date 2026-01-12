0331e3863ff7f7f681fbef67931d3112ba343d64
rish12345678 <101540285+rish12345678@users.noreply.github.com>
2026-01-12 20:43:33 +0000

    Boundary Tag Prog

--- mymalloc.c
+++ mymalloc.c
@@ -23,15 +23,15 @@ typedef struct block {
   bool is_alloc;      // false = free, true = allocated
   int magic_num;
   struct block *next; // Where is the next block in the free list
+  struct block *prev; // Where is the last block in the free list
 } block_t;
 
-// Think of the block struct as the header, at the start of every chunk of memory, free or not.
+// Think of the footer struct as the footer, at the end of every chunk of memory, free or not.
 typedef struct footer {
   size_t size;        // How many bytes beyond this metadata have been
                       // allocated for this block
   bool is_alloc;      // false = free, true = allocated
   int magic_num;
-  struct block *next; // Where is the next block in the free list
 } footer_t;
 
 block_t* head = NULL;
@@ -90,24 +90,31 @@ void insert_block(block_t *block) {
 }
 
 void *mymalloc(size_t s) {
+  // Given the user requests zero bytes, just return null
   if (s == 0) {
     return NULL;
   }
 
+  // If the page_size variable has not been set yet, set it to the systems page size
   if (page_size == 0) {
     page_size = sysconf(_SC_PAGE_SIZE);
   }
 
+  // Here we are checking that the size of the memory chunk we ought to return is aligned 
+  // to eight bytes on this 64-bit architecture.  We do this to minimize the number of 
+  // memory reads that the CPU has to do.  Giving out < 8 extra bytes per mymalloc is no 
+  // hassle especially when optimizing for latency.
   int remainder = s % 8;
 
   if (remainder != 0) {
     s = s + (8 - remainder);
   }
 
-  size_t small_block_limit = page_size - sizeof(block_t);
+  // Check to see if the requested memory will even fit in the page we will get via mmap
+  size_t small_block_limit = page_size - sizeof(block_t) - sizeof(footer_t);
 
   if (s > small_block_limit) {
-    size_t needed = s + sizeof(block_t);
+    size_t needed = s + sizeof(block_t) + sizeof(footer_t);
     size_t num_pages = (needed + page_size - 1) / page_size;
     size_t alloc_size = num_pages * page_size;
 
@@ -130,8 +137,16 @@ void *mymalloc(size_t s) {
     }
 
     block_t *block = (block_t*) mem_ptr;
-    block->size = alloc_size - sizeof(block_t);
+    block->size = alloc_size - sizeof(block_t) = sizeof(footer_t);
+    block->is_alloc = false;
+    block->magic_num = MAGIC_NUM;
     block->next = NULL;
+    block->prev = NULL;
+
+    footer_t *footer = (footer_t*) ((char*) mem_ptr + sizeof(block_t) + block->size);
+    footer-> size = alloc_size - sizeof(block_t) = sizeof(footer_t);
+    footer->is_alloc = false;
+    footer->magic_num = MAGIC_NUM;
 
     return (void*) (block + 1);
   }
@@ -140,23 +155,44 @@ void *mymalloc(size_t s) {
   block_t *curr = head;
   block_t *prev = NULL;
 
+  // Here we loop over the free list, checking all free chunks of memory headers
   while (curr) {
+    // If we find a chunk of free memory that is the size requested or larger....
     if (curr->size >= s) {
       debug_printf("malloc: block of size %zu found\n", curr->size);
 
+      // Make the free list skip over that chunk that is good enough to give to the user
+      prev = curr->prev;
       if (prev) {
         prev->next = curr->next;
       } else {
         head = curr->next;
       }
 
-      if (curr->size >= s + sizeof(block_t) + sizeof(block_t)) {
-        block_t *new_block = (block_t*)((char*)curr + sizeof(block_t) + s);
-        new_block->size = curr->size - s - sizeof(block_t);
+      
+      if (curr->size >= s + sizeof(block_t) + 8 + sizeof(footer_t)) {
+        block_t *block_for_extraneous_memory = (block_t*)((char*)curr + sizeof(block_t) + s + sizeof(footer_t));
+        footer_t *footer_for_return_memory = (footer_t*)((char*)curr + sizeof(block_t) + s);
+        footer_t *footer_for_extraneous_memory = (footer_t*)((char*)curr + sizeof(block_t) + curr->size);
+
+        footer_for_extraneous_memory->size = curr->size - s - sizeof(block_t) - sizeof(footer_t);
+        footer_for_extraneous_memory->is_alloc = false;
+
+        footer_for_return_memory->size = s;
+        footer_for_return_memory->is_alloc = true;
+        footer_for_return_memory->magic_num = MAGIC_NUM;
+
+        new_block->size = curr->size - s - sizeof(block_t) - sizeof(footer_t);
+        new_block->is_alloc = false;
+        new_block->magic_num = MAGIC_NUM;
+        new_block->prev = curr;
+        new_block->next = curr->next;
+
         
         debug_printf("malloc: splitting - blocks of size %zu and %zu created\n", s, new_block->size);
                      
         curr->size = s;
+        curr->is_alloc = true;
         insert_block(new_block);
       }
       return (void*) (curr + 1);
@@ -184,9 +220,10 @@ void *mymalloc(size_t s) {
   }
 
   block_t *block = (block_t*) new_heap_start;
-  block->size = page_size - sizeof(block_t);
+  block->size = page_size - sizeof(block_t) - sizeof(footer_t);
 
-  if (block->size >= s + sizeof(block_t) + sizeof(block_t)) {
+  // Check this
+  if (block->size >= s + sizeof(footer_t) + sizeof(block_t) + 8 + sizeof(footer_t)) {
     block_t *new_block = (block_t*)((char*)block + sizeof(block_t) + s);
     new_block->size = block->size - s - sizeof(block_t);
 
